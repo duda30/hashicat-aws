9814cc8d1c4a04ae0ea3f6ac3f517ebfb7913805
duda30 <dudalombardi30@gmail.com>
2022-10-08 17:48:52 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "ACME-mary"
     workspaces {
       name = "hashicat-aws"
